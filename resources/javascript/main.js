356326dc703416d3c55e88248d668d915fffe503
JaySoftwareSolutionsLLC <Brettbrewster@jaysoftwaresolutions.com>
2018-02-06 02:56:27 -0500

    getting How Much To Retire to print to console

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,6 +1,8 @@
 // $( document ).ready(function() {
     // "use strict";
 
+/*--- HISTORICAL DATA ---------------------------------------------------------------------------------------------------*/
+
     let sAndP = {
         '1950' : 31.71,
         '1951' : 24.02,
@@ -71,7 +73,195 @@
         '2016' : 11.96,
         '2017' : 21.83
     }
+    let valueOfDollar = {
+        '1950' : 1.00,
+        '1951' : 1.06,
+        '1952' : 1.12,
+        '1953' : 1.13,
+        '1954' : 1.14,
+        '1955' : 1.13,
+        '1956' : 1.14,
+        '1957' : 1.17,
+        '1958' : 1.20,
+        '1959' : 1.22,
+        '1960' : 1.25,
+        '1961' : 1.26,
+        '1962' : 1.27,
+        '1963' : 1.29,
+        '1964' : 1.31,
+        '1965' : 1.32,
+        '1966' : 1.35,
+        '1967' : 1.39,
+        '1968' : 1.44,
+        '1969' : 1.50,
+        '1970' : 1.60,
+        '1971' : 1.69,
+        '1972' : 1.74,
+        '1973' : 1.80,
+        '1974' : 1.96,
+        '1975' : 2.20,
+        '1976' : 2.35,
+        '1977' : 2.47,
+        '1978' : 2.63,
+        '1979' : 2.87,
+        '1980' : 3.25,
+        '1981' : 3.66,
+        '1982' : 3.98,
+        '1983' : 4.14,
+        '1984' : 4.29,
+        '1985' : 4.46,
+        '1986' : 4.63,
+        '1987' : 4.68,
+        '1988' : 4.89,
+        '1989' : 5.11,
+        '1990' : 5.34,
+        '1991' : 5.67,
+        '1992' : 5.84,
+        '1993' : 6.01,
+        '1994' : 6.18,
+        '1995' : 6.34,
+        '1996' : 6.50,
+        '1997' : 6.72,
+        '1998' : 6.83,
+        '1999' : 6.94,
+        '2000' : 7.13,
+        '2001' : 7.37,
+        '2002' : 7.49,
+        '2003' : 7.67,
+        '2004' : 7.81,
+        '2005' : 8.06,
+        '2006' : 8.34,
+        '2007' : 8.55,
+        '2008' : 8.90,
+        '2009' : 8.91,
+        '2010' : 9.15,
+        '2011' : 9.29,
+        '2012' : 9.56,
+        '2013' : 9.73,
+        '2014' : 9.87,
+        '2015' : 9.95,
+        '2016' : 10.02,
+        '2017' : 10.23
+    }
 
+    function adjustForInflation(curAmnt, curYr, compYr) {
+        return curAmnt * (valueOfDollar[compYr] / valueOfDollar[curYr]);
+    }
+    // console.log(adjustForInflation(43000, 1984, 1950));
+
+
+/* WITH ANNUAL CONTRIBUTIONS */
+    // let curAge = 23;
+    // let retAge = 50;
+    // let curInv = 15000;
+    let desLiv = 50000;
+
+    let minInv = 50000;
+    let maxInv = 1000000;
+    let invStp = 50000;
+
+    let minYrs = 5;
+    let maxYrs = 50;
+    let yrsStp = 5;
+    // let yrsInRetirement = 30
+
+    // Determine final balance of account starting at strtYr with $initInv, withdrawing $yrlyWithdrwl/yr for numYrs
+
+    let cases = {};
+
+    let inv = minInv;
+    for (inv; inv <= maxInv; inv += invStp) {
+        let years = minYrs;
+        let successByLength = [];
+        for (years; years <= maxYrs; years += yrsStp) {
+            let successRate = percentHistoricalBalancesAboveZero(inv, years)
+            successByLength.push(`${years} years : ${successRate}%`);
+        }
+        cases[inv] = successByLength;
+    }
+    console.log(cases);
+
+    function accountBalances(initInv, yrlyWithdrwl, strtYr, numYrs) {
+        let yr = strtYr;
+        let balances = [];
+        let balance = initInv;
+        let IAbalance;
+        let IAbalances = [];
+        for (yr; yr <= (strtYr + numYrs); yr++) {
+            balances.push(balance);
+            IAbalance = Math.floor(adjustForInflation(balance, yr, strtYr));
+            IAbalances.push(IAbalance);
+            let percentChange = sAndP[yr];
+            balance = Math.floor((balance - adjustForInflation(yrlyWithdrwl, strtYr, yr)) * (100 + percentChange) / 100);
+        }
+        return IAbalances;
+    }
+    function allBalancesAboveZero(balancesArray) {
+        let sortedBalances = balancesArray.sort(function(a,b) {
+            return a - b;
+        });
+        if (sortedBalances[0] > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    function percentHistoricalBalancesAboveZero(initInv, numYrs) {
+        let cases = [];
+        let yr = 1950;
+        for (yr; yr <= (2017 - numYrs); yr++) {
+            cases.push(allBalancesAboveZero(accountBalances(initInv, desLiv, yr, numYrs)));
+        }
+        let casesAsString = cases.join('');
+        let trueCases;
+        if (/true/.test(casesAsString)) {
+            trueCases = casesAsString.match(/true/g).length;
+        }
+        else {
+            trueCases = 0;
+        }
+        let totalCases = cases.length;
+        let percentage = Math.round((trueCases / totalCases) * 100);
+        return percentage;
+        // console.log(percentage);
+    }
+    // percentHistoricalBalancesAboveZero(300000, 50);
+    // console.log(accountBalances(500000, desLiv, 1960, yrsInRetirement));
+
+
+
+    function finalAccountBalance(initInv, yrlyWithdrwl, strtYr, numYrs) {
+        let yr = strtYr;
+        let balances = [];
+        let balance = initInv;
+        for (yr; yr < (strtYr + numYrs); yr++) {
+            let percentChange = sAndP[yr];
+            balance = (balance - yrlyWithdrwl) * (100 + percentChange) / 100;
+            balances.push(balance);
+        }
+        return balance;
+    }
+    function minAccountBalance(initInv, yrlyWithdrwl, strtYr, numYrs) {
+        let yr = strtYr;
+        let balances = [];
+        let balance = initInv;
+        for (yr; yr < (strtYr + numYrs); yr++) {
+            let percentChange = sAndP[yr];
+            balance = (balance - yrlyWithdrwl) * (100 + percentChange) / 100;
+            balances.push(balance);
+        }
+        balances.sort(function(a,b) {
+            return a - b;
+        })
+        return balances[0];
+    }
+
+
+
+/* NO ANNUAL CONTRIBUTIONS */
+
+    // Determine best and worst case scenerios letting money sit for X years
     function worstAndBestCases(investment, numYears) {
         function checkWorst(cur, yr) {
             if (cur < wrst) {
@@ -105,20 +295,32 @@
         let avgCase = cases.reduce(function(a, b) {
             return a + b;
         });
-        avgCase = avgCase / cases.length;
-        console.log(cases);
+        avgCase = Math.floor(avgCase / cases.length);
+        function median() {
+            if (numCases % 2 === 0) {
+                return cases[numCases / 2];
+            }
+            else {
+                return cases[(numCases - 1) / 2];
+            }
+        }
+        let medCase = median();
+        // console.log(cases);
         function CAGR(start, final, years) {
             return Math.round((((Math.pow((final / start), (1/years))) - 1) * 10000), 2) / 100;
         }
         let wrstCAGR = CAGR(investment, wrst, numYears);
         let bstCAGR = CAGR(investment, bst, numYears);
         let avgCAGR = CAGR(investment, avgCase, numYears);
+        let medCAGR = CAGR(investment, medCase, numYears);
 
         console.log(`Since 1950, the worst return on a ${investment}$ investment over ${numYears} years would have resulted in ${wrst}$ (${wrstYr}). This equates to an annualized Return of %${wrstCAGR}.
-        The best return would have resulted in ${bst}$ (${bstYr}), which equates to an annualized return of %${bstCAGR}.
-        The average return was ${avgCase} for an average annualized return of %${avgCAGR}`);
+The best return would have resulted in ${bst}$ (${bstYr}), which equates to an annualized return of %${bstCAGR}.
+The average return was ${avgCase} for an average annualized return of %${avgCAGR}.
+The median return was ${medCase} for a median annualized return of %${medCAGR}.
+*None of these values account for inflation`);
     }
-
+    // Determine the amount of $ at endYear if initialBalance was deposited and left untouched at startYear. Also tells the low and high points through time period.
     function historicalSummary(startYear, endYear, initialBalance) {
         function checkMin(cur, yr) {
             if (cur < min) {
@@ -149,8 +351,8 @@
         // console.log(`If you had invested ${initialBalance}$ in ${startYear} it would be worth ${balance} by the end of ${year}. The lowest value would have been ${min} at the end of ${minYr}. The highest value would have been ${max} at the end of ${maxYr}`)
         return balance;
     }
-    worstAndBestCases(150000, 20);
-    // historicalSummary(1955, 1965, 10000);
 
 
+    // worstAndBestCases(10000, 20);
+    historicalSummary(1955, 1965, 10000);
 // });
