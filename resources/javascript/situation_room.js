43c1484b8f6941dd7c4107877e44b41bfdf34ec3
JaySoftwareSolutionsLLC <Brettbrewster@jaysoftwaresolutions.com>
2018-02-23 15:32:54 -0500

    creating the front-end user inputs and js to create inputs and alter min/max

--- resources/javascript/situation_room.js
+++ resources/javascript/situation_room.js
@@ -1,42 +1,42 @@
 /*------------------------SIMULATED USER INPUTS-------------------------------*/
-let dob         = '1994,04,28';
-let dbt         = '18000';
-let aggInt      = '4.25';
-let inv_1       = '18000';
-let inv_acc_1   = 'sAndP';
-let inv_2       = '0';
-let inv_acc_2   = 'tenYearBond';
+// let dob         = '1994-04-28';
+// let dbt         = '18000';
+// let aggInt      = '4.25';
+// let inv_1       = '18000';
+// let inv_acc_1   = 'sAndP';
+// let inv_2       = '0';
+// let inv_acc_2   = 'tenYearBond';
 
 // let stp_1_start = 'now';
-let stp_1_start = dobToAge(dob);
-let stp_1_end   = 'paid-off';
-let stp_1_typ   = 'debt-repay';
-let stp_1_acc   = 'debts';
-let stp_1_amnt  = '6000';
-
-let stp_2_start = dobToAge(dob);
-let stp_2_end   = '27';
-let stp_2_typ   = 'investment';
-let stp_2_acc   = 'sAndP';
-let stp_2_amnt  = '15000';
-
-let stp_3_start = '27';
-let stp_3_end   = '35';
-let stp_3_typ   = 'investment';
-let stp_3_acc   = 'sAndP';
-let stp_3_amnt  = '25000';
-
-let stp_4_start = '50';
-let stp_4_end   = '60';
-let stp_4_typ   = 'withdraw';
-let stp_4_acc   = 'sAndP';
-let stp_4_amnt  = '30000';
-
-let stp_5_start = '60';
-let stp_5_end   = '80';
-let stp_5_typ   = 'withdraw';
-let stp_5_acc   = 'sAndP';
-let stp_5_amnt  = '70000';
+// let stp_1_start = dobToAge(dob);
+// let stp_1_end   = 'paid-off';
+// let stp_1_typ   = 'debt-repay';
+// let stp_1_acc   = 'debts';
+// let stp_1_amnt  = '6000';
+//
+// let stp_2_start = dobToAge(dob);
+// let stp_2_end   = '27';
+// let stp_2_typ   = 'investment';
+// let stp_2_acc   = 'sAndP';
+// let stp_2_amnt  = '15000';
+//
+// let stp_3_start = '27';
+// let stp_3_end   = '35';
+// let stp_3_typ   = 'investment';
+// let stp_3_acc   = 'sAndP';
+// let stp_3_amnt  = '25000';
+//
+// let stp_4_start = '50';
+// let stp_4_end   = '60';
+// let stp_4_typ   = 'withdraw';
+// let stp_4_acc   = 'sAndP';
+// let stp_4_amnt  = '30000';
+//
+// let stp_5_start = '60';
+// let stp_5_end   = '80';
+// let stp_5_typ   = 'withdraw';
+// let stp_5_acc   = 'sAndP';
+// let stp_5_amnt  = '70000';
 
 /*--------------------------PUSH DATA INTO OBJECTS----------------------------*/
 
@@ -45,28 +45,28 @@ let user = {
     'aggInt'    : Number(aggInt)
 }
 let accounts = {
-    'debts' : Number(dbt),
-    'sAndP' : Number(inv_1),
-    'tenYearBond' : Number(inv_2)
+    'debts' : 0,
+    'sAndP' : 0,
+    'tenYearBond' : 0
 }
 let steps = [];
-function Step(start, end, type, account, amount) {
+function Step(start, end, account, type, amount) {
     this.start = start,
     this.end = end,
-    this.type = type,
     this.account = account,
+    this.type = type,
     this.amount = Number(amount)
 }
-let step_1 = new Step(stp_1_start, stp_1_end, stp_1_typ, stp_1_acc, stp_1_amnt);
-let step_2 = new Step(stp_2_start, stp_2_end, stp_2_typ, stp_2_acc, stp_2_amnt);
-let step_3 = new Step(stp_3_start, stp_3_end, stp_3_typ, stp_3_acc, stp_3_amnt);
-let step_4 = new Step(stp_4_start, stp_4_end, stp_4_typ, stp_4_acc, stp_4_amnt);
-let step_5 = new Step(stp_5_start, stp_5_end, stp_5_typ, stp_5_acc, stp_5_amnt);
-steps.push(step_1);
-steps.push(step_2);
-steps.push(step_3);
-steps.push(step_4);
-steps.push(step_5);
+// let step_1 = new Step(stp_1_start, stp_1_end, stp_1_typ, stp_1_acc, stp_1_amnt);
+// let step_2 = new Step(stp_2_start, stp_2_end, stp_2_typ, stp_2_acc, stp_2_amnt);
+// let step_3 = new Step(stp_3_start, stp_3_end, stp_3_typ, stp_3_acc, stp_3_amnt);
+// let step_4 = new Step(stp_4_start, stp_4_end, stp_4_typ, stp_4_acc, stp_4_amnt);
+// let step_5 = new Step(stp_5_start, stp_5_end, stp_5_typ, stp_5_acc, stp_5_amnt);
+// steps.push(step_1);
+// steps.push(step_2);
+// steps.push(step_3);
+// steps.push(step_4);
+// steps.push(step_5);
 
 let allSimulations = [];
 
@@ -83,6 +83,11 @@ function dobToAge(dob) {
     let today = new Date().getTime();
     let birthday = new Date(dob).getTime();
     return Math.round((today - birthday) / (365.25 * 24 * 60 * 60 * 1000) * 100) / 100;
+}
+function dateToAge(dob, date) {
+    let birthTime = new Date(dob).getTime();
+    let dateTime = new Date(date).getTime();
+    return Math.round((dateTime - birthTime) / (365.25 * 24 * 60 * 60 * 1000) * 100) / 100;
 }
 function returnNetInf(netInf, annInf) {
     return Math.round(((((netInf + 100) / 100) * ((annInf + 100) / 100)) - 1) * 10000) / 100;
@@ -229,7 +234,7 @@ function returnMednHistoricalCase(allSimulationsArray) {
         }
     }
     return medianCase;
-}
+} //This might better be altered to determine median for EVERY year and plug that into a new array that it returns instead of just returning the median final value.
 function returnWrstHistoricalCase(allSimulationsArray) {
     const NUM_SIMS = allSimulationsArray.length;
     const LNG_OF_SIM = allSimulationsArray[0].length;
@@ -258,7 +263,6 @@ function returnBestHistoricalCase(allSimulationsArray) {
     }
     return bestCase;
 }
-
 function arrayToObjectWithAge(array) {
     let object = {};
     let i = 0;
@@ -273,138 +277,133 @@ function arrayToObjectWithAge(array) {
     return object;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-
-let startConditions = {
-    'initDebt'      : 0,
-    'aggIntRate'    : 4.2,
-    'initInv'       : 355137,
-    'mrkt'          : markets['sAndP']
-    // 355137
-}
-let timePeriod = {
-    // 'startYear'     : 1984,
-    'numYrs'        : 20,
-    'annDbtRepay'   : 0,
-    'annInv'        : 20000,
-    'mrkt'          : markets['sAndP']
+function assessPlan() {
+    fillInUser();
+    fillInAccounts();
+    fillInSteps();
+    runSimulation();
+    let wrst = arrayToObjectWithAge(returnWrstHistoricalCase(allSimulations));
+    let best = arrayToObjectWithAge(returnBestHistoricalCase(allSimulations));
+    let avrg = arrayToObjectWithAge(returnAvrgHistoricalCase(allSimulations));
+    let medn = arrayToObjectWithAge(returnMednHistoricalCase(allSimulations));
+    console.log(`Summary of all historical cases:`)
+    console.log(allSimulations);
+    console.log('Worst historical case:');
+    console.log(wrst);
+    console.log('Median historical case:');
+    console.log(medn);
+    console.log('Average historical case:');
+    console.log(avrg);
+    console.log('Best historical case:');
+    console.log(best);
 }
 
-function createCase(startingConditions, thisTimePeriod, startYear) {
-    let debt, inv, netWorth, yr, finalYr, netInf, intRate, thisCase, thisMarket, annInv, annRepay;
-    debt = startingConditions['initDebt'];
-    inv = startingConditions['initInv'];
-    netWorth = inv - debt;
-    yr = startYear;
-    finalYr = yr + thisTimePeriod['numYrs'];
-    netInf = 1;
-    intRate = startConditions['aggIntRate'];
-    thisCase = [];
-    thisMarket = thisTimePeriod['mrkt'];
-    annInv = thisTimePeriod['annInv'];
-    annRepay = timePeriod['annDbtRepay'];
-    restartAt = 2018;
-    for (yr; yr < finalYr; yr++) {
-        if (yr >= restartAt) {
-            let yearsSimulated = yr - startYear;
-            let yearsLeftToSimulate = thisTimePeriod['numYrs'] - yearsSimulated;
-            yr = 1950; // Needs to be restructured to not be hard coded. Will cause issues with anything that doesnt have data at 1950+
-            finalYr = yr + yearsLeftToSimulate;
-        }
-        let annInf = inflation[yr];
-        let mrktChng = thisMarket[yr];
-        netInf *= (100 + annInf) / 100;
-        netInf = Math.round(netInf * 10000) / 10000;
-        if (debt != 0) {
-            debt = debt * (intRate + 100) / 100;
-            debt -= annRepay;
-        }
-        if (debt <= 0) {
-            debt = 0;
-        }
-        inv = inv * (100 + mrktChng) / 100;
-        inv += annInv;
-        netWorth = Math.round((inv - debt) * 100) / 100;
-        IAnetWorth = Math.round(netWorth / netInf);
-        thisCase.push(IAnetWorth);
-        // console.log(`net inflation: ${netInf} | annual inflation: ${annInf} | market change: ${mrktChng}`)
+function inputToObject(inputHandle, object, property) {
+    let value = $(inputHandle).val();
+    object[property] = value;
+}
+function fillInUser() {
+    let dob = $('input#dob').val();
+    user['age'] = dobToAge(dob);
+    inputToObject('input#aggInt', user, 'aggInt');
+    // console.log(user);
+}
+function fillInAccounts() {
+    for (let prop in accounts) {
+        accounts[prop] = 0;
     }
-    return thisCase;
+    let dbt = Number($('input#debts').val());
+    accounts['debts'] = dbt;
+    let num_acnts = $('div.account').length;
+    let i = 1;
+    for (i; i <= num_acnts; i++) {
+        let accountName = $(`#acnt-mrkt-${i}`).val();
+        let accountVal = Number($(`#acnt-val-${i}`).val());
+        accounts[accountName] += accountVal;
+    }
+    // console.log(num_acnts);
+    // console.log(accounts);
+}
+function fillInSteps() {
+    steps = [];
+    let num_stps = $('div.step').length;
+    let i = 1;
+    for (i; i <= num_stps; i++) {
+        let stepStart = $(`#step-strt-${i}`).val();
+        let stepEnd = $(`#step-end-${i}`).val();
+        let stepMarket = $(`#step-mrkt-${i}`).val();
+        let stepType = $(`#step-type-${i}`).val();
+        let stepAmount = $(`#step-val-${i}`).val();
+        steps.push(new Step(stepStart, stepEnd, stepMarket, stepType, stepAmount));
+    }
+    // console.log(num_stps);
+    // console.log(steps);
 }
 
-let cases = [];
-for (let strtYr = 1950; strtYr <= 2017; strtYr++) {
-    cases.push(createCase(startConditions, timePeriod, strtYr));
-}
-cases = cases.sort(function(a, b) {
-    return a[a.length - 1] - b[b.length - 1];
-})
-*/
-/*
-function generateAvgCase(allCases) { /* NOT WORKING BUT BERGIE IS HERE SO FUCK IT WE'll BE BACK!!! */
-/*    let net = [0,0,0,0,0,0,0,0,0,0,0,0,0];
-    let i = 0;
-    let LNG = allCases[0].length;
-    for (i; i < LNG; i++) {
-        let thisCase = allCases[i];
-        for (let j = 0; j < LNG; j++) {
-            net[j] += thisCase[i];
-        }
-    }
-    return net;
-}
-console.log(generateAvgCase(cases));
-let bestCaseFinal = cases[cases.length - 1][cases[0].length - 1];
-let worstCaseFinal = cases[0][cases[0].length - 1]; */
-// console.log(`In the best historical case, you would have $${bestCaseFinal} by the end. In the worst historical case, you would've had $${worstCaseFinal}`);
-// console.log(cases);
-// console.log(createCase(startConditions, timePeriod));
+
+/*--------------------- USER INTERFACE -------------------------------*/
+let initialAccounts = 0;
+let initialPlans = 0;
+$('button.add-account').on('click', function() {
+    initialAccounts++;
+    $('button.add-account').before(`    <div class='account'>
+                                        <div class='input'>
+                    						<label for='acnt-val-${initialAccounts}'>Value</label>
+                    						<input type='number' id='acnt-val-${initialAccounts}' step="100" min="0">
+                    					</div>
+                    					<div class='input'>
+                    						<label for='acnt-mrkt-${initialAccounts}'>Market</label>
+                    						<select id='acnt-mrkt-${initialAccounts}'>
+                                                <option value='sAndP'>S and P 500</option>
+                                                <option value='tenYearBond'>10 Year Bond</option>
+                                            </select>
+                    					</div>
+                    				</div>`
+    )
+});
+$('button.add-step').on('click', function() {
+    initialPlans++;
+    let dob = $('#dob').val();
+    let age = dobToAge(dob);
+    $('button.add-step').before(`<div class='step'>
+                                        <div class='time-frame'>
+                                            <div class='input'>
+                        						<label for='step-strt-${initialPlans}'>Start (age)</label>
+                        						<input type='number' id='step-strt-${initialPlans}' min='${age}' max='90'>
+                        					</div>
+                                            <div class='input'>
+                                                <label for='step-end-${initialPlans}'>End (age)</label>
+                                                <input type='number' id='step-end-${initialPlans}' min='${age}' max='90'>
+                                            </div>
+                                        </div>
+                                        <div class='values'>
+                        					<div class='input'>
+                        						<label for='step-mrkt-${initialPlans}'>Market</label>
+                        						<select id='step-mrkt-${initialPlans}'>
+                                                    <option value='debts'>Debts</option>
+                                                    <option value='sAndP'>S and P 500</option>
+                                                    <option value='tenYearBond'>10 Year Bond</option>
+                                                </select>
+                        					</div>
+                                            <div class='input'>
+                        						<label for='step-type-${initialPlans}'>Type</label>
+                        						<select id='step-type-${initialPlans}'>
+                                                    <option value='debt-repay'>Repay Debt</option>
+                                                    <option value='investment'>Investment</option>
+                                                    <option value='withdraw'>Withdrawal</option>
+                                                </select>
+                        					</div>
+                                            <div class='input'>
+                                                <label for='step-val-${initialPlans}'>Amount</label>
+                                                <input type='number' id='step-val-${initialPlans}' step='50'>
+                                            </div>
+                                        </div>
+                    				</div>`
+    )
+});
+$('button.assess').on('click', function() {
+    // console.log('click');
+    assessPlan();
+});
+
+/*-------------------- RESTRICTING INPUTS ---------------------------*/
